c63dff03446bd6d7c87dbae26977b42a6915c787
Kussssssss <kusskus669@gmail.com>
2025-06-02 08:16:30 +0700

    initial commit

--- japan_dataset/run_get_covost_splits.bat
+++ japan_dataset/run_get_covost_splits.bat
@@ -1,7 +1,7 @@
 @echo off
-python "C:\Users\Admin\PycharmProjects\comsl-1\japan_dataset\get_covost_splits.py" ^
+python "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\japan_dataset\get_covost_splits.py" ^
     --version 2 ^
     --src-lang ja ^
     --tgt-lang en ^
-    --root "C:\Users\Admin\PycharmProjects\comsl-1\japan_dataset" ^
-    --cv-tsv "C:\Users\Admin\PycharmProjects\comsl-1\japan_dataset\validated.tsv" 
+    --root "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\japan_dataset" ^
+    --cv-tsv "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\japan_dataset\validated.tsv" 
--- welsh_dataset/run_get_covost_splits.bat
+++ welsh_dataset/run_get_covost_splits.bat
@@ -1,7 +1,7 @@
 @echo off
-python "C:\Users\Admin\PycharmProjects\comsl-1\welsh_dataset\get_covost_splits.py" ^
+python "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\welsh_dataset\get_covost_splits.py" ^
     --version 2 ^
     --src-lang cy ^
     --tgt-lang en ^
-    --root "C:\Users\Admin\PycharmProjects\comsl-1\welsh_dataset" ^
-    --cv-tsv "C:\Users\Admin\PycharmProjects\comsl-1\welsh_dataset\validated.tsv" 
+    --root "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\welsh_dataset" ^
+    --cv-tsv "C:\Users\Admin\PycharmProjects\comsl_ja_cy_id\welsh_dataset\validated.tsv" 
